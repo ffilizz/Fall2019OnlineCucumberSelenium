f4052368580efbf909f7b9623cb1e544709b990c
ffilizz <filizikibas@gmail.com>
2020-04-18 15:27:00 -0500

    maven cucumber html report/screenshot

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,8 +10,12 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@activities",
-        plugin = "html:target/cucumber-html-report"
+        tags = "",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber.json"
+        }
+
 )
 public class CucumberRunner {
 
