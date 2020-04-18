6e7a3712c347d6748eab89cc9c58ed443fbc97f6
ffilizz <filizikibas@gmail.com>
2020-04-18 12:56:26 -0500

    created new feature file: CreateCalendarEvent.feature

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@login_with_params ",
+        tags = "@activities",
         plugin = "html:target/cucumber-html-report"
 )
 public class CucumberRunner {
